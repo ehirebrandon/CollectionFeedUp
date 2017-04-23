3734e1b2287088f4a5bd61afdcb3826b2843dde6
Brandon Lai <brandon@brandons-mbp.attlocal.net>
2017-04-23 14:19:12 -0400

    Instantiate ViewController without storyboard, so programatically

--- CollectionFeed/AppDelegate.swift
+++ CollectionFeed/AppDelegate.swift
@@ -16,6 +16,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        
+        let flowLayout = UICollectionViewFlowLayout()
+        let customCollectionViewController = CustomCollectionViewController(collectionViewLayout: flowLayout)
+        
+        window?.rootViewController = UINavigationController(rootViewController: customCollectionViewController)
+        
         // Override point for customization after application launch.
         return true
     }
--- CollectionFeed/ViewController.swift
+++ CollectionFeed/ViewController.swift
@@ -8,10 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CustomCollectionViewController: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        collectionView?.backgroundColor = UIColor.blue
         // Do any additional setup after loading the view, typically from a nib.
     }
 
