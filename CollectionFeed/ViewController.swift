cba4bd33232df0c29375410713c0f157c689d12c
Brandon Lai <brandon@brandons-mbp>
2017-04-24 22:07:38 -0400

    added more code

--- CollectionFeed/ViewController.swift
+++ CollectionFeed/ViewController.swift
@@ -8,21 +8,60 @@
 
 import UIKit
 
-class CustomCollectionViewController: UICollectionViewController {
+class CustomCollectionViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+    
+    let customCellIdentifier = "customCellIdentifier"
 
     override func viewDidLoad() {
         super.viewDidLoad()
-     
         
         collectionView?.backgroundColor = UIColor.blue
+        
+                collectionView?.register(CustomCell.self, forCellWithReuseIdentifier: customCellIdentifier)
+
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let customCell = collectionView.dequeueReusableCell(withReuseIdentifier: customCellIdentifier, for: indexPath)
+        
+        return customCell
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
 
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+}
 
-
+class CustomCell: UICollectionViewCell{
+    override init(frame: CGRect){
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Custom Text"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    func setupView(){
+        addSubview(nameLabel)
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": nameLabel]))
+             addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": nameLabel]))
+    }
+    
+    required init?(coder aDecoder: NSCoder){
+        fatalError("init(coder:) has not been implemented")
+    }
 }
 
